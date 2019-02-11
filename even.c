97dea9508f9ba56260c5aeff6b90b4b3b3c02a52
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-11 19:12:10 +0530

    Update even.c

--- even.c
+++ even.c
@@ -3,14 +3,13 @@ int main()
 {
     int i,n;
     scanf("%d",&n);
-    printf("enter the even num bt 6 to  9  \n",n);
-    for(i=8;i<=9;i++)
+    printf("enter the even num bt 1to 10  \n",n);
+    for(i=1;i<10;i++)
     {
         if(i%2==0)
         {
         printf("%d",i);
         }
     }
-    return 0;
     
 }
